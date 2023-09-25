833631d1d19080fecdb813132a3adee2e4c9017e
Claudia Andrea <andrea8040@hotmail.com>
2023-09-25 15:52:03 -0500

    new changes

--- PageObject/HomeObject.py
+++ PageObject/HomeObject.py
@@ -1,5 +1,4 @@
 import time
-from lib2to3.pgen2 import driver
 
 from selenium.webdriver.common.by import By
 
@@ -14,7 +13,10 @@ class HomePageObject:
         self.form_email = "email"
         self.form_phone = "phone"
         self.form_address = "textarea"
-        self.check_gender = "male"
+        self.wikipedia_search = "Wikipedia1_wikipedia-search-input"
+        self.search_button = "wikipedia-search-button"
+        self.browser_windows_button = "//button[@onclick='myFunction()']"
+
 
     def get_first_name(self):
         return self.driver.find_element(By.ID, self.form_name)
@@ -28,20 +30,33 @@ class HomePageObject:
     def get_form_address(self):
         return self.driver.find_element(By.ID, self.form_address)
 
-    #def get_scroll_down(self):
+    def get_wikipedia_search(self):
+        return self.driver.find_element(By.ID, self.wikipedia_search)
+
+    def get_search_button(self):
+        return self.driver.find_element(By.CLASS_NAME, self.search_button)
+
+    def get_browser_windows(self):
+        return self.driver.find_element(By.XPATH, self.browser_windows_button)
+
      #   flag = driver.find_element(By.ID, self.form_address)
       #  driver.execute_script("arguments(0).scrollIntoView();",flag)
 
     #def get_check_gender(self):
+
      #   return self.driver.find_element(By.ID, self.check_gender)
 
-    def sign_up(self, form_name, form_mail, form_phone, form_address):
+    def sign_up(self, form_name, form_mail, form_phone, form_address, wikipedia_search):
         self.get_first_name().send_keys(form_name)
         self.get_form_email().send_keys(form_mail)
         self.get_form_phone().send_keys(form_phone)
         self.get_form_address().send_keys(form_address)
+        self.get_wikipedia_search().send_keys(wikipedia_search)
+        self.get_search_button().click()
         time.sleep(2)
-        #self.get_check_gender().click()
+        self.get_browser_windows().click()
+        #
+        self.driver.execute_script("windows.scrollTo(0,document.body.scrollHeight)")
 
     #def form_check(self):
      #   self.get_check_gender().click()
--- TestPages/HomePage.py
+++ TestPages/HomePage.py
@@ -11,6 +11,6 @@ class Test_course_pack(WebDriverSetup):
         self.driver.get(HomePageObject.get_base_url())
         home_object = HomePageObject(driver)
         time.sleep(3)
-        home_object.sign_up("daniel", "andrea8040@hotmail.com", "3135355651", "Calle 47f nro 85 a 25")
+        home_object.sign_up("daniel", "andrea8040@hotmail.com", "3135355651", "Calle 47f nro 85 a 25", "monday")
         #home_object.check_gender()
        # scroll.distance =1000
